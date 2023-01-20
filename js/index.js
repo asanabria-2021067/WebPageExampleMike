14b6a496030cf9542af0559f09d404d7236b9201
Michael Salvatierra <msalvatierra-2021150@kinal.edu.gt>
2023-01-20 08:59:11 -0600

    Primer commit V1 Webpage

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+function saludar() {
+    console.log('Ciudad Peronia');
+    let num1 = document.getElementById("numero1").value;
+    let num2 = document.getElementById("numero2").value;
+    let resultado = parseInt(num1) + parseInt(num2);
+    console.log(num1);
+    console.log(num2);
+console.log(resultado);
+}
